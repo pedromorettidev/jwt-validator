24ece98ae819a17dc41c2ad060465b8d2bea53f0
pedromorettidev <pedromorettidesenvolvedor@gmail.com>
2026-06-01 18:35:09 -0300

    feature: add Dockerfile, ci yml to github actions and logs to improve observability

--- src/main/kotlin/com/example/jwtvalidator/api/controller/TokenValidatorController.kt
+++ src/main/kotlin/com/example/jwtvalidator/api/controller/TokenValidatorController.kt
@@ -1,6 +1,7 @@
 package com.example.jwtvalidator.api.controller
 
 import com.example.jwtvalidator.application.service.TokenValidatorService
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -17,6 +18,12 @@ class TokenValidatorController(
     fun validateJwt(
         @RequestParam jwt: String
     ): ResponseEntity<Boolean> {
+        logger.info("$LOG_PREFIX: Received jwt validation request.")
         return ResponseEntity.status(HttpStatus.OK).body(tokenValidatorService.validateJwtToken(jwt))
+    }
+
+    companion object {
+        private val LOG_PREFIX = TokenValidatorController::class.simpleName
+        private val logger = LoggerFactory.getLogger(TokenValidatorController::class.java)
     }
 }
--- src/main/kotlin/com/example/jwtvalidator/application/service/TokenValidatorService.kt
+++ src/main/kotlin/com/example/jwtvalidator/application/service/TokenValidatorService.kt
@@ -2,6 +2,7 @@ package com.example.jwtvalidator.application.service
 
 import com.example.jwtvalidator.application.decoder.JwtDecoder
 import com.example.jwtvalidator.domain.TokenClaims
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import kotlin.math.sqrt
 
@@ -10,20 +11,37 @@ class TokenValidatorService(
     private val jwtDecoder: JwtDecoder
 ) {
     fun validateJwtToken(jwt: String): Boolean {
+        logger.info("$LOG_PREFIX: Validating token claims.")
         val claimsData = runCatching {
             jwtDecoder.decode(jwt)
         }.getOrElse {
+            logger.warn("$LOG_PREFIX: Error when trying to decode JWT. Message: ${it.message}")
             return false
         }
 
-        if (claimsData.claimsCount != REQUIRED_CLAIMS_COUNT) return false
+        logger.info("$LOG_PREFIX: Decoded jwt succesfully with claims count: ${claimsData.claimsCount}")
 
-        if(!hasAllRequiredClaims(claimsData)) return false
+        if (claimsData.claimsCount != REQUIRED_CLAIMS_COUNT) {
+            logger.info("$LOG_PREFIX: Claims count is not $REQUIRED_CLAIMS_COUNT. Invalid JWT Token.")
+            return false
+        }
 
-        if (!isValidName(claimsData.name!!)) return false
+        if(!hasAllRequiredClaims(claimsData)) {
+            logger.info("$LOG_PREFIX: Don't have all required claims. Invalid JWT Token.")
+            return false
+        }
 
-        if (!isPrime(claimsData.seed!!)) return false
+        if (!isValidName(claimsData.name!!)) {
+            logger.info("$LOG_PREFIX: Name field ${claimsData.name} is invalid. Invalid JWT Token.")
+            return false
+        }
 
+        if (!isPrime(claimsData.seed!!)) {
+            logger.info("$LOG_PREFIX: Seed number is not a prime number. Invalid JWT Token.")
+            return false
+        }
+
+        logger.info("$LOG_PREFIX: Token succesfully validated")
         return true
     }
 
@@ -53,11 +71,15 @@ class TokenValidatorService(
     }
 
     private companion object {
+
         const val REQUIRED_CLAIMS_COUNT = 3
         const val MAX_NAME_LENGTH = 256
 
         const val EVEN_PRIME_NUMBER = 2L
         const val FIRST_ODD_DIVISOR = 3
         const val DIVISOR_STEP = 2
+
+        private val logger = LoggerFactory.getLogger(TokenValidatorService::class.java)
+        val LOG_PREFIX = TokenValidatorService::class.simpleName
     }
 }
